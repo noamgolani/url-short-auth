1ee59a788fb7951686210099c55c6e8d1a72e78b
noamgolani <noam.golani@gmail.com>
2021-11-02 13:36:26 +0200

    validates url

--- __test__/app.test.ts
+++ __test__/app.test.ts
@@ -1,11 +1,20 @@
-import request from 'supertest';
-import app from '../src/app'
+import request from "supertest";
+import app from "../src/app";
 
-describe("POST /api/shorten " ,()=>{
-	test("Should validate the url body param",async ()=> {
-		const response = await request(app).post('/api/shorten');
-		expect(response.statusCode).toBe(400);
-		expect(response.body.error).toBe("Missing url param");
+describe("POST /api/shorten ", () => {
+  test("Should validate the url body param", async () => {
+    const response = await request(app).post("/api/shorten");
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe("Missing url param");
+  });
+
+	test("Should validate that the url is url", async () => {
+    const response1 = await request(app).post("/api/shorten").send({url: 'Im not a valid url'});
+    expect(response1.statusCode).toBe(400);
+    expect(response1.body.error).toBe("Not a url");
+    const response2 = await request(app).post("/api/shorten").send({url: 'www.google'});
+    expect(response2.statusCode).toBe(400);
+    expect(response2.body.error).toBe("Not a url");
 	})
 
-})
+});
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import validUrl from 'valid-url';
 const app = express();
 
 app.use(express.json());
@@ -6,6 +7,10 @@ app.use(express.json());
 app.post("/api/shorten", (req, res) => {
   const { url } = req.body;
   if (!url) throw { status: 400, message: "Missing url param" };
+	if(!validUrl.isUri(url)) throw {status: 400, message: "Not a url"};
+	else res.send(200)
+	res.end();
+	
 });
 
 app.use((err, req, res, next) => {
@@ -15,6 +20,8 @@ app.use((err, req, res, next) => {
       error: err.message,
     });
   }
+
+	res.end();
 });
 
 export default app;
