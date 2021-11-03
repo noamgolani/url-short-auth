07f5bf513adabcca5e87ac2a63df2740dab6a544
noamgolani <noam.golani@gmail.com>
2021-11-03 22:18:18 +0200

    style

--- src/routes/appRoute.ts
+++ src/routes/appRoute.ts
@@ -2,11 +2,16 @@ import path from 'path';
 import express from 'express';
 const router = express.Router();
 
+router.get('/stats/:uid',(req,res) => {
+  // will fetch data
+  // will return template
+});
 
 router.get("/", (_, res) => {
   res.sendFile(path.resolve("./public/index.html"));
 });
 
 router.use("/", express.static(path.resolve("./public")));
+
 
 export default router;
