8e2008087269053551e2ed10a637dfeec5ff4c3e
noamgolani <noam.golani@gmail.com>
2021-11-07 09:04:02 +0200

    readme

--- src/routes/shortRoute.ts
+++ src/routes/shortRoute.ts
@@ -1,16 +1,15 @@
-
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import { updateStats, getUrl } from '../lib/dummyDB';
+import { updateStats, getUrl } from "../lib/dummyDB";
 
 router.get("/:uid", async (req, res, next) => {
   const { uid } = req.params;
   console.log(uid);
-  
+
   try {
     const url = await getUrl(uid);
-    if (!url) return res.redirect('/app');
+    if (!url) return res.redirect("/app/");
     await updateStats(uid);
     res.redirect(url);
   } catch (error) {
@@ -18,8 +17,8 @@ router.get("/:uid", async (req, res, next) => {
   }
 });
 
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
   res.redirect("/app");
-})
+});
 
 export default router;
